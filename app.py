09cbc2bd177dcf402fe1cb12639f2118d530ad48
Anne Chiang <anneannechiang@gmail.com>
2022-07-29 16:33:18 +0800

    update

--- app.py
+++ app.py
@@ -38,9 +38,16 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
+    r = '不清楚您在說什麼耶..'
+
+    if msg == '嗨':
+        r = '嗨'
+    elif msg == '你吃飯了嗎':
+        r = '當然囉'
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text='Okayyy....'))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
